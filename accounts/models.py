3759bc0d61a4addd029f72b3793f6784b4db30ac
Nadia Mofidi <nadiamofidiwebsite@gmail.com>
2026-08-28 15:19:01 +0330

    created Profile model and added signal for creating a profile for each user when signed in

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    job_title = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(
+        upload_to='profiles/',
+        blank=True,
+        null=True
+    )
+    facebook = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    behance = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
