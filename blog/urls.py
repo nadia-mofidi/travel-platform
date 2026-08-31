ca118295efebe1d72be3337a42d0769bb53033b1
Nadia Mofidi <nadiamofidiwebsite@gmail.com>
2026-08-31 14:30:56 +0330

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,12 @@ from blog.views import *
 app_name='blog'
 urlpatterns = [
     path("",blog_view, name='index'),
-    path("<int:pid>/",blog_single, name='single'),
     path("category/<str:cat_name>/",blog_view,name='category'),
     path("author/<str:auth_username>/",blog_view,name='author'),
-    path("search/",blog_search,name='search'),
     path("tags/<str:tag_name>/",blog_view,name='tags'),
+    path("<int:pid>/",blog_single, name='single'),
+    path("search/",blog_search,name='search'),
+    
     path("dashboard/", author_dashboard, name="dashboard"),
     path("dashboard/create/",create_post,name="create_post"),
 ]
