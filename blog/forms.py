59ee0d598a7b3d3b7597d21bfc3deb963a8480da
Nadia Mofidi <nadiamofidiwebsite@gmail.com>
2026-08-31 12:45:18 +0330

    created postForm in blog for author dashboard

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
-from django import forms 
-from blog.models import Comment
+from django import forms
+from blog.models import Comment,Post
 from captcha.fields import CaptchaField
+from django_summernote.widgets import SummernoteWidget
 
 class CommentForm(forms.ModelForm):
     
@@ -8,3 +9,23 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['post',"name","email","subject","message",]
         
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+
+        fields = [
+            'title',
+            'content',
+            'image',
+            'category',
+            'tags',
+            'login_require',
+            'publish_date',
+            'status',
+        ]
+
+        widgets = {
+            'content': SummernoteWidget(),
+        }
