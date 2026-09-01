56f1f83478c3e249a4bb2fab316d4b609df1634f
Nadia Mofidi <nadiamofidiwebsite@gmail.com>
2026-09-01 21:17:53 +0330

    Optimize category post counting with annotations

--- blog/templatetags/blog_temptags.py
+++ blog/templatetags/blog_temptags.py
@@ -1,6 +1,8 @@
 from django import template
 from blog.models import Post,Category,Comment
 from django.utils import timezone
+from django.db.models import Count, Q
+from django.utils import timezone
 register = template.Library()
 
 @register.inclusion_tag('blog/blog-popular-post.html')
@@ -10,12 +12,11 @@ def popularposts():
 
 @register.inclusion_tag('blog/blog-post-categories.html')
 def postcategories():
-    posts = Post.objects.filter(status=1,publish_date__lte=timezone.now())
-    categories=Category.objects.all()
-    cat_dict={}
-    for cat in categories:
-        cat_dict[cat]=posts.filter(category=cat).count()
-    return {'categories':cat_dict}
+    categories = Category.objects.annotate(
+        post_count=Count('post',filter=Q(post__status=1,post__publish_date__lte=timezone.now()))
+    )
+   
+    return {'categories': categories}
 
 # @register.simple_tag(name='comments_count')
 # def function(pid):
