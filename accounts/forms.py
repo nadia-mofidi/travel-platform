661f7dbd06392ff818d087c1b8d9a981d7206a7c
Nadia Mofidi <nadiamofidiwebsite@gmail.com>
2026-09-03 16:12:17 +0330

    updated some fields
    name last name are not required now

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 class SignupForm(UserCreationForm):
-    first_name = forms.CharField(max_length=150, required=True)
-    last_name = forms.CharField(max_length=150, required=True)
+    first_name = forms.CharField(max_length=150, required=False)
+    last_name = forms.CharField(max_length=150, required=False)
     email = forms.EmailField(required=True)
 
     class Meta:
